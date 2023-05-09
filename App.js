96de255ad7f2af0d692f9307ed08089d08f9797f
Yuriy Minashkin <y.minashkin@gmail.com>
2023-05-10 01:22:23 +0300

    upd

--- App.js
+++ App.js
@@ -31,32 +31,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-// import { StatusBar } from "expo-status-bar";
-// import { StyleSheet, View, ImageBackground } from "react-native";
-// import { RegistrationScreen } from "./Screens/RegistrationScreen";
-// import { LoginScreen } from "./Screens/LoginScreen";
-// import BackGroundImage from "./assets/images/back-ground.png";
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <StatusBar style="auto" />
-//       <ImageBackground source={BackGroundImage} style={styles.bgImage}>
-//         <RegistrationScreen />
-//         {/* <LoginScreen /> */}
-//       </ImageBackground>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   bgImage: {
-//     flex: 1,
-//     resizeMode: "cover",
-//     justifyContent: "flex-end",
-//   },
-// });
